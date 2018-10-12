3f3d4b5fb071db2f72dd88e6b5b22ed17d80a9f1
NSz <nick.szewczak@gmail.com>
2018-10-12 15:26:50 -0400

    starting out

---
+++ test-ascii.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Nick Szewczak
+Course: CSCI-136
+Instructor: Subhadarshi Panda
+Assignment: Lab 5 task A
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
